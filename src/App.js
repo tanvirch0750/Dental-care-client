d9c8c7f773c61cb0f81bc08889f70f053d774e79
tanvirch0750 <tanvir0750@yahoo.com>
2022-05-10 01:52:18 +0600

    add google fonts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello dental care</h1>
+      <h1 className="text-6xl">Hello dental care</h1>
       <button className="btn btn-primary">Button</button>
     </div>
   );
